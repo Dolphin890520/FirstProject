5e31e07400f9362aa3e5690bf5676d1a4878d162
Dolphin8950512 <zz3353aa@yahoo.com.tw>
2024-02-19 15:10:36 +0800

    更新系統以及安裝Tool

--- IPerf_2_Prots.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash  
-
-echo 請稍後
-
-sleep 5
-
-echo 完成，即將退出
-
---
+++ InstallTool.sh
@@ -0,0 +1,13 @@
+#!/bin/bash  
+
+echo  
+
+echo "正在安裝所需要的套件，請稍後。"
+
+apt-get install update -y
+apt install iperf -y
+apt install fio -y
+apt install ipmitool -y
+
+echo "已安裝好測試Tool"
+
